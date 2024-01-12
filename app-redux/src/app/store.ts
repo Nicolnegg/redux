4291045c80651b0834150e423111ce0517cc5800
Nicolnegg <princesnegg@gmail.com>
2024-01-12 18:43:40 -0500

    creacion

---
+++ app-redux/src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+import counterReducer from "../features/counter/counterSlice"
+import headerReducer from "../features/header/headerSlice"
+
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    header: headerReducer,
+  },
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
